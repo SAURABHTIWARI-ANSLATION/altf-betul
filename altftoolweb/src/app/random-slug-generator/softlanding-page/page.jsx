1d3ba9146db86df1dbb44f9777a4a49294a77560
Nikita Munje <nikita.munje@anslation.com>
2026-05-30 12:20:48 +0530

    Fix softlanding page issues and update MaisonLanding components

--- altftoolweb/src/app/random-slug-generator/softlanding-page/page.jsx
+++
@@ -1,7 +0,0 @@
-"use client"
-import React from 'react'
-import MaisonLanding from '../components/softlanding-page/MaisonLanding'
-
-export default function SoftLandingPageRoute() {
-  return <MaisonLanding />
-}
